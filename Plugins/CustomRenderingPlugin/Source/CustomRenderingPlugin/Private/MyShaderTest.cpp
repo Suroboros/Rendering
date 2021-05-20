1e0e7a98a5930e0f74c927f8d08e2b8b83e10142
Suroboros <caixiao.stu@gmail.com>
2021-05-20 16:59:28 +0900

    Uniform Buffer
    Send parameter to shader by uniform buffer(const buffer)

--- Plugins/CustomRenderingPlugin/Source/CustomRenderingPlugin/Private/MyShaderTest.cpp
+++ Plugins/CustomRenderingPlugin/Source/CustomRenderingPlugin/Private/MyShaderTest.cpp
@@ -5,6 +5,19 @@
 
 #define LOCTEXT_NAMESPACE "CustomRenderingModul"
 
+//-----------------------------
+// Uniform Buffer(Const Buffer)
+//-----------------------------
+BEGIN_GLOBAL_SHADER_PARAMETER_STRUCT(FMyShaderParameterBuffer, )
+SHADER_PARAMETER(FVector4, MyColor1)
+SHADER_PARAMETER(FVector4, MyColor2)
+SHADER_PARAMETER(FVector4, MyColor3)
+SHADER_PARAMETER(FVector4, MyColor4)
+SHADER_PARAMETER(uint32, ColorIndex)
+END_GLOBAL_SHADER_PARAMETER_STRUCT()
+
+IMPLEMENT_GLOBAL_SHADER_PARAMETER_STRUCT(FMyShaderParameterBuffer, "MyShaderParameter");
+
 
 //-----------------------------
 // Global Shader Class
@@ -29,17 +42,22 @@ public:
 	FMyShaderTest(const ShaderMetaType::CompiledShaderInitializerType& Initializer) : FGlobalShader(Initializer)
 	{
 		SimpleColorVal.Bind(Initializer.ParameterMap, TEXT("SimpleColor"));
+		SimpleTextureVal.Bind(Initializer.ParameterMap, TEXT("SimpleTexture"));
+		SimpleTextureSamplerVal.Bind(Initializer.ParameterMap, TEXT("SimpleTextureSampler"));
 	}
 
-	template<typename TShaderRHIParamRef>
-	void SetParameters(FRHICommandListImmediate& RHICmdList, const TShaderRHIParamRef Shader, const FLinearColor& MyColor)
+	template<typename TRHIShader>
+	void SetParameters(FRHICommandListImmediate& RHICmdList, TRHIShader Shader, const FLinearColor& MyColor, FTexture2DRHIRef MyTexture)
 	{
 		SetShaderValue(RHICmdList, Shader, SimpleColorVal, MyColor);
+		SetTextureParameter(RHICmdList, Shader, SimpleTextureVal, SimpleTextureSamplerVal, TStaticSamplerState<SF_Trilinear, AM_Clamp>::GetRHI(), MyTexture);
 	}
 
 private:
 	// Use LAYOUT_FIELD after ver.4.25
 	LAYOUT_FIELD(FShaderParameter, SimpleColorVal);
+	LAYOUT_FIELD(FShaderResourceParameter, SimpleTextureVal);
+	LAYOUT_FIELD(FShaderResourceParameter, SimpleTextureSamplerVal);
 };
 
 class FMyShaderTestVS : public FMyShaderTest
@@ -69,36 +87,83 @@ IMPLEMENT_SHADER_TYPE(, FMyShaderTestPS, TEXT("/MyShaders/Private/MyTestShader.u
 //-----------------------------
 // Data struct
 //-----------------------------
+struct FTextureVertex
+{
+	FVector4    Position;
+	FVector2D   UV;
+};
 
 class FQuadVertexBuffer : public FVertexBuffer
 {
 public:
 	void InitRHI() override
 	{
-		TResourceArray<FVector4, VERTEXBUFFER_ALIGNMENT> Vertices;
-		Vertices.SetNumUninitialized(4);
-		Vertices[0] = FVector4(-1.0f, 1.0f, 0, 1.0f);
-		Vertices[1] = FVector4(1.0f, 1.0f, 0, 1.0f);
-		Vertices[2] = FVector4(-1.0f, -1.0f, 0, 1.0f);
-		Vertices[3] = FVector4(1.0f, -1.0f, 0, 1.0f);
+		TResourceArray<FTextureVertex, VERTEXBUFFER_ALIGNMENT> Vertices;
+		Vertices.SetNumUninitialized(6);
+		
+		// Left up
+		Vertices[0].Position = FVector4(1.0f, 1.0f, 0, 1.0f);
+		Vertices[0].UV = FVector2D(1.0f, 1.0f);
+
+		// Right up
+		Vertices[1].Position = FVector4(-1.0f, 1.0f, 0, 1.0f);
+		Vertices[1].UV = FVector2D(0, 1.0f);
+
+		// Left bottom
+		Vertices[2].Position = FVector4(1.0f, -1.0f, 0, 1.0f);
+		Vertices[2].UV = FVector2D(1.0f, 0);
+
+		// Right bottom
+		Vertices[3].Position = FVector4(-1.0f, -1.0f, 0, 1.0f);
+		Vertices[3].UV = FVector2D(0, 0);
+
+		// For the triangle optimization
+		Vertices[4].Position = FVector4(-1.0f, 1.0f, 0, 1.0f);
+		Vertices[4].UV = FVector2D(-1.0f, 1.0f);
+
+		Vertices[5].Position = FVector4(1.0f, -1.0f, 0, 1.0f);
+		Vertices[5].UV = FVector2D(1.0f, -1.0f);
 
 		FRHIResourceCreateInfo CreateInfo(&Vertices);
 		VertexBufferRHI = RHICreateVertexBuffer(Vertices.GetResourceDataSize(), BUF_Static, CreateInfo);
 	}
 };
+
 class FQuadIndexBuffer : public FIndexBuffer
 {
 public:
 	void InitRHI() override
 	{
-		const uint16 Indices[] = { 0, 1, 2, 2, 1, 3 };
+		const uint16 Indices[] = { 0, 1, 2, 2, 1, 3, 0, 4, 5 };
 
 		TResourceArray<uint16, INDEXBUFFER_ALIGNMENT> IndexBuffer;
-		IndexBuffer.AddUninitialized(6);
-		FMemory::Memcpy(IndexBuffer.GetData(), Indices, 6 * sizeof(uint16));
+		uint32 IndicesCount = UE_ARRAY_COUNT(Indices);
+		IndexBuffer.AddUninitialized(IndicesCount);
+		FMemory::Memcpy(IndexBuffer.GetData(), Indices, IndicesCount * sizeof(uint16));
 
+		// Create index buffer.
 		FRHIResourceCreateInfo CreateIndexInfo(&IndexBuffer);
 		IndexBufferRHI = RHICreateIndexBuffer(sizeof(uint16), IndexBuffer.GetResourceDataSize(), BUF_Static, CreateIndexInfo);
+	}
+};
+
+class FTextureVertexDeclaration : public FRenderResource
+{
+public:
+	FVertexDeclarationRHIRef  VertexDeclarationRHI;
+
+	virtual void InitRHI() override
+	{
+		FVertexDeclarationElementList Elements;
+		uint32 Stride = sizeof(FTextureVertex);
+		Elements.Add(FVertexElement(0, STRUCT_OFFSET(FTextureVertex, Position), VET_Float2, 0, Stride));
+		Elements.Add(FVertexElement(0, STRUCT_OFFSET(FTextureVertex, UV), VET_Float2, 1, Stride));
+		VertexDeclarationRHI = RHICreateVertexDeclaration(Elements);
+	}
+
+	virtual void ReleaseRHI() override
+	{
+		VertexDeclarationRHI->Release();
 	}
 };
 
@@ -106,7 +171,7 @@ public:
 // Blueprint
 //-----------------------------
 
-static void DrawMyShaderTestRenderTarget_RenderThread(FRHICommandListImmediate& RHICmdList, FTextureRenderTargetResource* OutputRenderTargetResource, ERHIFeatureLevel::Type FeatureLevel, FLinearColor MyColor, FName TextureRenderTargetName)
+static void DrawMyShaderTestRenderTarget_RenderThread(FRHICommandListImmediate& RHICmdList, FTextureRenderTargetResource* OutputRenderTargetResource, ERHIFeatureLevel::Type FeatureLevel, FLinearColor MyColor, FTextureRHIRef MyTexture, FMyShaderParameterStruct MyShaderStructData, FName TextureRenderTargetName)
 {
 	check(IsInRenderingThread());
 
@@ -115,7 +180,7 @@ static void DrawMyShaderTestRenderTarget_RenderThread(FRHICommandListImmediate& 
 	TextureRenderTargetName.ToString(EventName);
 	SCOPED_DRAW_EVENTF(RHICmdList, SceneCapture, TEXT("ShaderTest %s"), *EventName);
 #else
-	SCOPED_DRAW_EVENT(RHICmdList, DrawUVDisplacementToRenderTarget_RenderThread);
+	SCOPED_DRAW_EVENT(RHICmdList, DrawMyShaderTestRenderTarget_RenderThread);
 #endif
 
 	// Set Render Target
@@ -129,9 +194,20 @@ static void DrawMyShaderTestRenderTarget_RenderThread(FRHICommandListImmediate& 
 	RHICmdList.BeginRenderPass(RPInfo, TEXT("MyShaderTest"));
 	{
 		// Get shaders.
+		FeatureLevel = GMaxRHIFeatureLevel;
 		FGlobalShaderMap* GlobalShaderMap = GetGlobalShaderMap(FeatureLevel);
 		TShaderMapRef<FMyShaderTestVS> VertexShader(GlobalShaderMap);
 		TShaderMapRef<FMyShaderTestPS> PixelShader(GlobalShaderMap);
+
+		// Get Data
+		FQuadVertexBuffer quadVertex;
+		quadVertex.InitRHI();
+		FQuadIndexBuffer quadIndex;
+		quadIndex.InitRHI();
+
+		// Texture vertex
+		FTextureVertexDeclaration VertexDeclaration;
+		VertexDeclaration.InitRHI();
 
 		// Set the graphic pipeline state.
 		FGraphicsPipelineStateInitializer GraphicsPSOInit;
@@ -141,7 +217,7 @@ static void DrawMyShaderTestRenderTarget_RenderThread(FRHICommandListImmediate& 
 		GraphicsPSOInit.RasterizerState = TStaticRasterizerState<>::GetRHI();
 		GraphicsPSOInit.PrimitiveType = PT_TriangleList;
 		// Bind Sahder
-		GraphicsPSOInit.BoundShaderState.VertexDeclarationRHI = GetVertexDeclarationFVector4();
+		GraphicsPSOInit.BoundShaderState.VertexDeclarationRHI = VertexDeclaration.VertexDeclarationRHI;
 		GraphicsPSOInit.BoundShaderState.VertexShaderRHI = VertexShader.GetVertexShader();
 		GraphicsPSOInit.BoundShaderState.PixelShaderRHI = PixelShader.GetPixelShader();
 
@@ -151,14 +227,18 @@ static void DrawMyShaderTestRenderTarget_RenderThread(FRHICommandListImmediate& 
 		FIntPoint DisplacementMapResolution(RenderTargetTexture->GetSizeX(), RenderTargetTexture->GetSizeY());
 		RHICmdList.SetViewport(0, 0, 0.f, DisplacementMapResolution.X, DisplacementMapResolution.Y, 1.f);
 
-		// Update shader uniform parameters.
-		PixelShader->SetParameters(RHICmdList, PixelShader.GetPixelShader(), MyColor);
+		// Set uniform buffer
+		FMyShaderParameterBuffer MyShaderParameter;
+		MyShaderParameter.MyColor1 = MyShaderStructData.MyColor1;
+		MyShaderParameter.MyColor2 = MyShaderStructData.MyColor2;
+		MyShaderParameter.MyColor3 = MyShaderStructData.MyColor3;
+		MyShaderParameter.MyColor4 = MyShaderStructData.MyColor4;
+		MyShaderParameter.ColorIndex = MyShaderStructData.ColorIndex;
 
-		// Get Data
-		FQuadVertexBuffer quadVertex;
-		quadVertex.InitRHI();
-		FQuadIndexBuffer quadIndex;
-		quadIndex.InitRHI();
+		// Update shader uniform parameters.
+		SetUniformBufferParameterImmediate(RHICmdList, PixelShader.GetPixelShader(), PixelShader->GetUniformBufferParameter<FMyShaderParameterBuffer>(), MyShaderParameter);
+		VertexShader->SetParameters(RHICmdList, PixelShader.GetPixelShader(), MyColor, MyTexture->GetTexture2D());
+		PixelShader->SetParameters(RHICmdList, PixelShader.GetPixelShader(), MyColor, MyTexture->GetTexture2D());
 
 		RHICmdList.SetStreamSource(0, quadVertex.VertexBufferRHI, 0);
 
@@ -168,7 +248,13 @@ static void DrawMyShaderTestRenderTarget_RenderThread(FRHICommandListImmediate& 
 	RHICmdList.EndRenderPass();
 }
 
-void UMyShaderTestBlueprintLibrary::DrawMyShaderTestRenderTarget(UTextureRenderTarget2D* OutputRenderTarget, AActor* Actor, FLinearColor MyColor)
+void UMyShaderTestBlueprintLibrary::DrawMyShaderTestRenderTarget(
+	class UTextureRenderTarget2D* OutputRenderTarget,
+	AActor* Actor,
+	FLinearColor MyColor,
+	UTexture* MyTexture,
+	FMyShaderParameterStruct MyShaderStructData
+)
 {
 	check(IsInGameThread());
 
@@ -180,13 +266,15 @@ void UMyShaderTestBlueprintLibrary::DrawMyShaderTestRenderTarget(UTextureRenderT
 	const FName TextureRenderTargetName = OutputRenderTarget->GetFName();
 	FTextureRenderTargetResource* TextureRenderTargetResource = OutputRenderTarget->GameThread_GetRenderTargetResource();
 
+	FTextureRHIRef MyTextureRHI = MyTexture->TextureReference.TextureReferenceRHI;
+
 	ERHIFeatureLevel::Type FeatureLevel = Actor->GetWorld()->Scene->GetFeatureLevel();
 
 	// Put render command into render thread
 	ENQUEUE_RENDER_COMMAND(CaptureCommand)(
-		[TextureRenderTargetResource, FeatureLevel, MyColor, TextureRenderTargetName](FRHICommandListImmediate& RHICmdList)
+		[TextureRenderTargetResource, FeatureLevel, MyColor, MyTextureRHI, MyShaderStructData, TextureRenderTargetName](FRHICommandListImmediate& RHICmdList)
 		{
-			DrawMyShaderTestRenderTarget_RenderThread(RHICmdList, TextureRenderTargetResource, FeatureLevel, MyColor, TextureRenderTargetName);
+			DrawMyShaderTestRenderTarget_RenderThread(RHICmdList, TextureRenderTargetResource, FeatureLevel, MyColor, MyTextureRHI, MyShaderStructData, TextureRenderTargetName);
 		}
 	);
 }
--- Plugins/CustomRenderingPlugin/Source/CustomRenderingPlugin/Public/MyShaderTest.h
+++ Plugins/CustomRenderingPlugin/Source/CustomRenderingPlugin/Public/MyShaderTest.h
@@ -5,11 +5,39 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "MyShaderTest.generated.h"
 
+
+USTRUCT(BlueprintType)
+struct FMyShaderParameterStruct
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, meta = (WorldContext = "WorldContextObject"))
+		FLinearColor MyColor1;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, meta = (WorldContext = "WorldContextObject"))
+		FLinearColor MyColor2;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, meta = (WorldContext = "WorldContextObject"))
+		FLinearColor MyColor3;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, meta = (WorldContext = "WorldContextObject"))
+		FLinearColor MyColor4;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, meta = (WorldContext = "WorldContextObject"))
+		int32 ColorIndex;
+};
+
 UCLASS(MinimalAPI, meta=(ScriptName="CustomRenderingLibary"))
 class UMyShaderTestBlueprintLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 
 	UFUNCTION(BlueprintCallable, Category = "CustomRenderingPlugin", meta = (WorldContext = "WorldContextObject"))
-		static void DrawMyShaderTestRenderTarget(class UTextureRenderTarget2D* OutputRenderTarget, AActor * Actor, FLinearColor myColor);
+	static void DrawMyShaderTestRenderTarget(
+		class UTextureRenderTarget2D* OutputRenderTarget,
+		AActor * Actor,
+		FLinearColor MyColor,
+		UTexture* MyTexture,
+		FMyShaderParameterStruct MyShaderStructData
+	);
 };
